61214d0af798fff072bfb77a42fef6eadc90f5b3
rediumareanca <rediumareanca@ya.ru>
2017-06-29 09:10:02 +0300

    connection initialized

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from elasticsearch_dsl.connections import connections
 
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        connections.create_connection()
